4d813a6be071d3b221f46de15c456c00ec393077
Calvin Bonomo <cal.bonomo@gmail.com>
2025-09-26 11:07:21 -0600

    Added clear function and fixed assignment issue

--- src/utils.h
+++ src/utils.h
@@ -4,6 +4,7 @@
 typedef unsigned int uint;
 typedef unsigned long ulong;
 typedef unsigned short ushort;
+typedef unsigned char uchar;
 
 inline void *copy(void *src, void *dst, ulong n) {
   if (!src || !dst) return (void *)0;
@@ -16,15 +17,24 @@ inline void *copy(void *src, void *dst, ulong n) {
 }
 
 inline int swap(void *p1, void *p2, ulong n) {
-  if (!p1 || !p2 || !n) return -1;
+  if (!p1 || !p2) return -1;
 
   char *pByte1 = p1, *pByte2 = p2;
-  for (int i = 0; i < n; i++) {
+  for (ulong i = 0; i < n; i++) {
     pByte1[i] ^= pByte2[i];
     pByte2[i] ^= pByte1[i];
     pByte1[i] ^= pByte2[i];
   }
   return 0;
+}
+
+inline void *clear(void *p, ulong n) {
+  if (!p) return NULL;
+
+  for (ulong i = 0; i < n; i++) {
+    *((char *)p + i) = (char)0;
+  }
+  return p;
 }
 
 #endif
