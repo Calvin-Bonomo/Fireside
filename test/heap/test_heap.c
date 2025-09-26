c1fdd0d1d81ba05b3b9fd383139356e0721b85be
Calvin Bonomo <cal.bonomo@gmail.com>
2025-09-26 11:41:21 -0600

    Added tests for heap_init and heap_free

--- test/heap/test_heap.c
+++ test/heap/test_heap.c
@@ -1,5 +1,45 @@
 #include "test_heap.h"
 
+// Helper functions
+uint minCompare(void *left, void *right) {
+  int *ipLeft = left, 
+      *ipRight = right;
+  return *ipRight - *ipLeft;
+}
+
 void test_heap() {
-  
+  test_heap_init();
+  test_heap_free();
+}
+
+void test_heap_init() {
+  Heap heap;
+
+  // Fail cases
+  assert(heap_init(&heap, 0, minCompare) < 0);
+  assert(heap_init(NULL, 5, minCompare) < 0);
+  assert(heap_init(&heap, 5, NULL) < 0);
+
+  // Success case
+  assert(!heap_init(&heap, 5, minCompare));
+  assert(heap.count == 0);
+  assert(heap.maxCount == 5);
+  assert(heap.compare == minCompare);
+  assert(heap.data != NULL);
+
+  heap_free(&heap);
+}
+
+void test_heap_free() {
+  Heap heap;
+  heap_init(&heap, 5, minCompare);
+
+  // Fail case
+  assert(heap_free(NULL) < 0);
+
+  // Success case
+  assert(!heap_free(&heap));
+  for (ulong i = 0; i < sizeof(Heap); i++) {
+    assert(*(char *)&heap == 0);
+  }
 }
--- test/heap/test_heap.h
+++ test/heap/test_heap.h
@@ -2,7 +2,11 @@
 #define TEST_HEAP
 
 #include "heap.h"
+#include "assert.h"
 
 void test_heap();
+
+void test_heap_init();
+void test_heap_free();
 
 #endif
