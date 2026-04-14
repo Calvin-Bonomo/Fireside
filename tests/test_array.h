0ff3f4d32822cab6bf09452c18861b59883cd170
Calvin Bonomo <cal.bonomo@gmail.com>
2026-04-13 23:00:10 -0600

    Finished tests for array

--- tests/test_array.h
+++ tests/test_array.h
@@ -10,8 +10,24 @@
 void test_array()
 {
     array arr;
-
+    
+    // Test array_init
     TEST_MALLOC_FAIL(array_init(&arr, TEST_ARRAY_SIZE, sizeof(int)), 1, "array init fail");
+
+    TEST_EQ(array_init(&arr, TEST_ARRAY_SIZE, sizeof(int)), 0, "array init success");
+
+    // Test array set
+    for (uint i = 0; i < TEST_ARRAY_SIZE; i++)
+        array_set(&arr, &i, i);
+
+    for (uint i = 0; i < TEST_ARRAY_SIZE; i++)
+        TEST_EQ(((int *)arr.data)[i], i, "array get");
+
+    // Test array get
+    for (uint i = 0; i < TEST_ARRAY_SIZE; i++)
+        TEST_EQ(*(int *)array_get(&arr, i), (int)i, "array set");
+
+    array_free(&arr);
 }
 
 #endif
